c8c95350b80f490a89f7af3de344755343062ce6
hamiltcs2 <hamiltcs2@gmail.com>
2021-12-20 08:35:05 -0500

    initial commit server

--- server.js
+++ server.js
@@ -46,16 +46,16 @@ app.use(express.urlencoded({extended: false}));
 //HTTP request logger
 app.use(morgan('tiny'));
 app.use('/api', routes);
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
 
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) // since we're on local windows
-      url = url.substring(1);
-    res.sendFile(url);
-  });
+// app.get("*", (req, res) => {
+//     let url = path.join(__dirname, '../client/build', 'index.html');
+//     if (!url.startsWith('/app/')) // since we're on local windows
+//       url = url.substring(1);
+//     res.sendFile(url);
+//   });
 
